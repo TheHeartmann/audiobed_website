22d3e22473234b6a957cc303efeb00599afbc30a
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-05-19 15:59:00 +0200

    Adds ignore dirs to file copying script and renames it.
    Updates build script to follow.

--- build_process/copy_files_for_dist.py
+++ build_process/copy_files_for_dist.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-"""
+#!/usr/bin/env python3
+'''
 A script to move all contents of the frontend directory to a dist directory and
 rename the files to '<file>@<commit>.<extension>', where commit is the short
 hash of the last commit wherein the file was changed.
-"""
+'''
 
 import argparse
 import os
@@ -18,15 +18,17 @@ class FileStatus():
         self.revision = revision
 
     def name_at_rev(self):
-        return "{}@{}".format(self.name, self.revision)
+        return f'{self.name}@{self.revision}'
 
 
-def get_file_status(directory, extensions):
+def get_file_status(directory, extensions, ignore):
     file_status = []
-    for dirpath, _, files in os.walk(directory):
-        for f in [x for x in files if os.path.splitext(x)[1] in extensions]:
-            rev = os.popen('git log -n 1 --pretty=format:%h -- {}'.format(
-                os.path.join(dirpath, f))).read().strip()
+    for dirpath, dirs, files in os.walk(directory):
+        dirs[:] = [d for d in dirs if not re.match(ignore, d)]
+        for f in [x for x in files if os.path.splitext(x)[1] in extensions and not re.match(ignore, x)]:
+            rev = os.popen(
+                f'git log -n 1 --pretty=format:%h -- {os.path.join(dirpath, f)}'
+            ).read().strip()
             relative_path = os.path.sep.join(
                 os.path.join(dirpath, f).split(os.path.sep)[1:])
             file_status.append(FileStatus(relative_path, rev))
@@ -42,7 +44,7 @@ def rename_files(dir, *file_statuses):
 
             os.rename(path(fs.name), path(fs.name_at_rev()))
     except Exception as e:
-        print('Something went wrong while renaming files: {}'.format(e))
+        print(f'Something went wrong while renaming files: {e}')
         return False
     return True
 
@@ -65,23 +67,29 @@ def rename_references(dir, *file_statuses):
             with open(path, 'w', encoding='utf8') as file:
                 file.write(replace(content))
 
+def ignore(pattern):
+    def _ignore(_, content):
+        return [x for x in content if re.match(pattern, x)]
+    return _ignore
 
-def main(root_dir, target_dir, *extensions):
+
+def main(root_dir, target_dir, extensions, ignore_pattern):
     origin = os.getcwd()
     try:
         os.chdir(root_dir)
-        file_status_list = get_file_status(
-            'frontend', ['.{}'.format(x) for x in extensions])
+        file_status_list = get_file_status('frontend',
+                                           [f'.{x}' for x in extensions], ignore_pattern)
 
         if os.path.exists(target_dir):
             shutil.rmtree(target_dir)
-        shutil.copytree('frontend', target_dir)
+        shutil.copytree(
+            'frontend', target_dir, ignore=ignore(ignore_pattern))
         if rename_files(target_dir, *file_status_list):
             rename_references(target_dir, *file_status_list)
             rename_references(
                 os.path.join('backend', 'src'), *file_status_list)
     except Exception as e:
-        print('Something went terribly wrong: {}'.format(e))
+        print(f'Something went terribly wrong: {e}')
         sys.exit(1)
     finally:
         os.chdir(origin)
@@ -100,7 +108,10 @@ if __name__ == '__main__':
     parser.add_argument(
         '-e',
         '--extensions',
-        help='The file types to copy and treat.',
+        help='The file types to change.',
         default='js css html')
+    parser.add_argument(
+        '-i', '--ignore', help='Patterns to ignore', default='elm')
+
     args = parser.parse_args()
-    main(args.root, args.target, *args.extensions.split())
+    main(args.root, args.target, args.extensions.split(), ignore_pattern=args.ignore)
